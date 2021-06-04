27a656151222b75061c795747c2e87a30c613903
OH-JOO-YEONG <brb1111@naver.com>
2021-06-04 17:39:51 +0900

    파이썬 스터디 3문제 - 탑 | 백준 - 구간 합 구하기 | 백준 - 주지수 | 백준

---
+++ PythonStudy/06/1주차/11660_210603_ohzz.py
@@ -0,0 +1,28 @@
+# 구간 합 구하기 11660 백준
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+board = []
+for i in range(n):
+    board.append(list(map(int, input().split())))
+
+# 행 더하기
+for i in range(n):
+    for j in range(1, n):
+        board[i][j] += board[i][j - 1]
+
+# 열 더하기
+for i in range(n):
+    for j in range(1, n):
+        board[j][i] += board[j - 1][i]
+
+board.insert(0, [0] * n)
+
+for i in board:
+    i.insert(0, 0)
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(board[x2][y2] - board[x1 - 1][y2] - board[x2][y1 - 1] + board[x1 - 1][y1 - 1])
