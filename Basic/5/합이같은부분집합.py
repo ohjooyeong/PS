5985bed50ef2845bacff9dacf69e0142e21f86c8
OH-JOO-YEONG <brb1111@naver.com>
2021-06-21 23:17:01 +0900

    인프런 1문제 - 합이 같은 부분 집합

---
+++ Basic/5/합이같은부분집합.py
@@ -0,0 +1,46 @@
+n = int(input())
+arr = list(map(int, input().split()))
+check = [0] * n
+ans = "NO"
+
+
+def dfs(v):
+    global ans
+    if n == (v + 1):
+        c1 = []
+        c2 = []
+        for i in range(n):
+            if check[i] == 1:
+                c1.append(arr[i])
+            else:
+                c2.append(arr[i])
+        if sum(c1) == sum(c2):
+            ans = "YES"
+    else:
+        check[v] = 1
+        dfs(v + 1)
+        check[v] = 2
+        dfs(v + 1)
+
+
+dfs(0)
+print(ans)
+
+# import sys
+
+
+# def dfs(L, sum):
+#     if L == n:
+#         if sum == (total - sum):
+#             print("YES")
+#             sys.exit(0)
+#         else:
+#             dfs(L + 1, sum + arr[L])
+#             dfs(L + 1, sum)
+
+
+# if __name__ == "__main__":
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#     total = sum(a)
+#     dfs(0, 0)
